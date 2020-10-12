71f598475bc5008bf2cdc13070625b7c7abf9cf3
99002447 <71481211+99002447@users.noreply.github.com>
2020-10-12 09:41:45 +0530

    Create pal_test.py

---
+++ pal_test.py
@@ -0,0 +1,13 @@
+import unittest
+from pal import isPalindrome
+# import palindromeCheck
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(isPalindrome('abc'), 0)
+       # self.assertEqual(isPalindrome('aba'), 1)
+       # self.assertEqual(isPalindrome('123'), 0)
+       # self.assertEqual(isPalindrome('121'), 1)
+        
+if __name__ == '__main__':
+    unittest.main()
