674d7b9deb892b9ed032997801aa42f4e596eafc
99002447 <71481211+99002447@users.noreply.github.com>
2020-10-14 16:16:03 +0530

    Create test_facebook.py

---
+++ test_facebook.py
@@ -0,0 +1,15 @@
+import unittest
+from facebook import status
+# import palindromeCheck
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(status('Done'), Done)
+        #self.assertEqual(status('Done'), 1)
+        #self.assertEqual(isPalindrome('123'), 0)
+        #self.assertEqual(isPalindrome('121'), 1)
+        #self.assertEqual(add(5,7), 12)
+        #self.assertEqual(add(5,7), 15)
+        
+if __name__ == '__main__':
+    unittest.main()
