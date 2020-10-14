3c17b49cf40bea8ef35f74e3a74cc8e738bba12d
99002447 <71481211+99002447@users.noreply.github.com>
2020-10-14 11:46:39 +0530

    Update pal_test.py

--- pal_test.py
+++ pal_test.py
@@ -9,7 +9,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(isPalindrome('123'), 0)
         self.assertEqual(isPalindrome('121'), 1)
         self.assertEqual(add(5,7), 12)
-        self.assertEqual(add(5,7), 15)
+        #self.assertEqual(add(5,7), 15)
         
 if __name__ == '__main__':
     unittest.main()
