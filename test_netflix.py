b61529f336657dc97a93b864712ea92249a11899
99002447 <71481211+99002447@users.noreply.github.com>
2020-10-15 14:21:36 +0530

    Update and rename test_facebook.py to test_netflix.py

--- test_netflix.py
+++ test_netflix.py
@@ -1,10 +1,10 @@
 import unittest
-from facebook import status
+from netflix import status
 # import palindromeCheck
 
 class MyTest(unittest.TestCase):
     def test_my_function(self):
-        self.assertEqual(status(), "Done")
+        self.assertEqual(status(), "Finished")
         #self.assertEqual(status('Done'), 1)
         #self.assertEqual(isPalindrome('123'), 0)
         #self.assertEqual(isPalindrome('121'), 1)
